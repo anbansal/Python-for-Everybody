05857ac6ac778fb29536b7f09ec546d572d2def9
Ankur <ankurbnsl8099@gmail.com>
2019-10-03 16:28:52 -0400

    Chapter 4 : Functions -- Doing the exercises at end of the chapter

---
+++ Ch4-Functions/excerciseEndOfChp2.py
@@ -0,0 +1,30 @@
+def computegrade(scoreNum):
+    if scoreNum > 1.0 or scoreNum < 0.0:
+        print("Bad score")
+    elif scoreNum >= 0.9:
+        print("A")
+    elif scoreNum >= 0.8:
+        print("B")
+    elif scoreNum >= 0.7:
+        print("C")
+    elif scoreNum >= 0.6:
+        print("D")
+    elif scoreNum < 0.6:
+        print("F")
+
+
+print("Score\tGrade")
+print(">=0.9\tA")
+print(">=0.8\tB")
+print(">=0.7\tC")
+print(">=0.6\tD")
+print("<0.6\tF")
+
+prompt = "Enter score: "
+score = input(prompt)
+try:
+    scoreNum = float(score)
+except:
+    print("Bad score")
+    quit(0)
+computegrade(scoreNum)
