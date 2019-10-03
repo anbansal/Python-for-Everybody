ad6a9138ca945f8acfba8d299558c7e459fbe9d9
Ankur <ankurbnsl8099@gmail.com>
2019-10-03 10:06:03 -0400

    Doing Chapter 2 exercises

---
+++ Ch2-Variables-expressions-and-statements/PythonCode.py
@@ -0,0 +1,7 @@
+promt = "What is your first name?\n"
+firstName = input(promt)
+promt = "second name?\n"
+secondName = input(promt)
+promt = "Type your age\n"
+age = input(promt)
+print("In 10 years ",firstName,secondName," would be ", int(age) + 10, " years old.")
