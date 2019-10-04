f276c51747ed84479378892ede89ed2cf46d423f
Bergmeister <ankurbnsl8099@gmail.com>
2019-10-04 16:12:46 -0400

    Update exercise5EndOfChp.py

--- Ch8-Lists/exercise5EndOfChp.py
+++ Ch8-Lists/exercise5EndOfChp.py
@@ -13,7 +13,7 @@ def countFrom(fhand):
 
 def openFile():
     while True:
-        fname = input("Type your file name: ")
+        fname = input("Type your file name: ") #enter mbox-short.txt
         try:
             fhand = open(fname)
             break
