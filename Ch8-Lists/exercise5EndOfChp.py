b29492dfc193f93c8d19cc5dda065e605f80580a
Ankur <ankurbnsl8099@gmail.com>
2019-10-04 15:57:20 -0400

    Chapter 8 : Lists -- Exercise 5 at the end of the chapter

---
+++ Ch8-Lists/exercise5EndOfChp.py
@@ -0,0 +1,30 @@
+def countFrom(fhand):
+    count = 0
+    for line in fhand:
+        if line.startswith('From'):
+            if line.startswith('From:'): continue
+            try:
+                print((line.split())[1])
+            except:
+                continue
+            count = count + 1
+    print("There were", count, "lines in the file with From as the first word")
+
+
+def openFile():
+    while True:
+        fname = input("Type your file name: ")
+        try:
+            fhand = open(fname)
+            break
+        except:
+            print("Could not open the file", fname)
+            print("Please try again!")
+            continue
+
+    return fhand
+
+
+if __name__ == '__main__':
+    fhand = openFile()
+    countFrom(fhand)
