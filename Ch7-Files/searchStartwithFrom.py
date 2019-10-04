b13e6c0badeeb0015e21d6f161c5b9e5dc9ea67b
Ankur <ankurbnsl8099@gmail.com>
2019-10-04 13:17:53 -0400

    Chapter 7: Files -- Searching the file content

---
+++ Ch7-Files/searchStartwithFrom.py
@@ -0,0 +1,15 @@
+def search(str):
+    count = 0
+    fhand = open('mbox-short.txt')
+    #content = fhand.read()
+    for line in fhand:
+        if line.startswith(str):
+            print(line)
+            count = count + 1
+
+    print("The total no of lines which start with", str, "are ", count)
+
+
+if __name__ == '__main__':
+    str = input("Type your search pattern: ")
+    search(str)
